178cafe879d3275ad95963df4600602789719309
snnapier <snnapier@iu.edu>
2022-04-23 15:43:39 -0400

    add item and package

---
+++ src/edu/iu/c212/models/Item.java
@@ -0,0 +1,41 @@
+package edu.iu.c212.models;
+
+public class Item
+{
+    // instance variables
+    private String name;
+    private double price;
+    private int quantity;
+    private int aisleNum;
+
+    // constructor
+    public Item(String name, double price, int quantity, int aisleNum)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.aisleNum = aisleNum;
+    }
+
+    // getters
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    public int getAisleNum()
+    {
+        return aisleNum;
+    }
+
+}
