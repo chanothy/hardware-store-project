3aeff7d4817f16b0afeb4747e2decfe5832ae5f7
snnapier <snnapier@iu.edu>
2022-04-25 13:46:15 -0400

    finished getItemsFromFile --> Store class

--- src/edu/iu/c212/Store.java
+++ src/edu/iu/c212/Store.java
@@ -2,6 +2,10 @@ package edu.iu.c212;
 
 import edu.iu.c212.models.Item;
 import edu.iu.c212.models.Staff;
+import edu.iu.c212.utils.FileUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Store implements IStore
@@ -17,7 +21,17 @@ public class Store implements IStore
 
     public List<Item> getItemsFromFile()
     {
+        List<Item> inventoryArrList = new ArrayList<Item>();
+        try {
+            // use FileUtils
+            inventoryArrList = FileUtils.readInventoryFromFile();
 
+        } catch(IOException e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return inventoryArrList;
     }
 
     public List<Staff> getStaffFromFile()
